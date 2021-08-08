d41242d250f34e4d41fa56c4e3edb21b577786a3
alexgordontlv <alexaybn@gmail.com>
2021-08-08 23:24:43 +0300

    l

--- controller.js
+++ controller.js
@@ -1,11 +1,20 @@
 class Controller {
 	constructor() {
-		this.model = new Calculator();
+		this.model = null;
+		this.view = null;
+	}
+	calculator_options = {
+		standard: () => new Calculator('standart'),
+		science: () => new Calculator('science'),
+	};
+	buildCalculatorView = (calculatorType) => {
+		console.log(calculatorType);
+		this.model = new Calculator(calculatorType);
 		this.view = new View(this.model.operations);
-		this.view.bindEventClick(this.model.calc);
-	}
 
-	run() {
+		this.view.bindEventClick(this.model.calc);
+	};
+	run = () => {
 		this.view.render();
-	}
+	};
 }
