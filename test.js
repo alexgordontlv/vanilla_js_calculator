7cf88b47d911905d5cc7a3cbb8da34c10b71b4be
alexgordontlv <alexaybn@gmail.com>
2021-08-13 11:18:06 +0300

    test change

--- test.js
+++ test.js
@@ -6,11 +6,4 @@ class Test {
 			throw new Error(`In function ${testFunction.name} expected ${expected} , result: ${result}`);
 		}
 	}
-	testRender({ testFunction, expected, params }) {
-		console.log(...params);
-		const result = testFunction(...params);
-		if (a !== expected) {
-			throw new Error(`In function ${testFunction.name} expected ${expected} , result: ${result}`);
-		}
-	}
 }
